7b5e842dd6203a466e0b08af33125d4679650a11
trannhon70 <kevintran351996@gmail.com>
2026-04-23 09:36:42 +0700

    update

--- layout/header_layout.php
+++ layout/header_layout.php
@@ -14,9 +14,43 @@
         </div>
     </div>
     <div style="position: relative;" class="header__mobile-baner">
-        <img fetchpriority=high src="<?php echo $local ?>/images/banner/banner_mobile.webp" alt="..." srcset="">
+        <!-- <img fetchpriority=high src="<?php echo $local ?>/images/banner/banner_mobile.webp" alt="..." srcset="">
         <div style="position: absolute; bottom: 0; left: 0; width: 100%; height: 15vh; display: flex;">
             <a style="display: block ; width: 100%; height: 15vh;" href="javascript:void(0)" onclick="openZoosUrl('chatwin'); return false;"></a>
+        </div> -->
+
+        <div class="carousel" id="carousel">
+            <div class="carousel-track">
+
+                <!-- Slide 1 (LCP) -->
+                <div class="slide">
+                    <div style="position:relative;width:100%;max-width:100%;">
+                        <img
+                            src="<?php echo $local ?>/images/banner/banner_mobile.webp"
+                            fetchpriority="high"
+                            decoding="async"
+                            width="1920"
+                            height="600"
+                            style="width:100%;object-fit:cover;"
+                            alt="banner phòng khám">
+                    </div>
+                </div>
+                <!-- Slide 1 (LCP) -->
+                <div class="slide">
+                    <div style="position:relative;width:100%;max-width:100%;">
+                        <img
+                            src="<?php echo $local ?>/images/banner/3.webp"
+                            decoding="async"
+                            width="1920"
+                            height="600"
+                            style="width:100%;object-fit:cover;"
+                            alt="banner phòng khám">
+                    </div>
+                </div>
+            </div>
+            <button style=" display:none;" class="nav prev">❮</button>
+            <button style="display:none;" class="nav next">❯</button>
+            <div class="dots"></div>
         </div>
     </div>
 </header>
